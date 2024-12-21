3c769c5fdf56882dca2f7979452d9b01aff6acc0
zhengzhoucoder <zhengzhou@52it.net>
2024-12-21 15:35:17 +0800

    init: remove red point for zhihu and bilibili
    Signed-off-by: zhengzhoucoder <zhengzhou@52it.net>

---
+++ script.js
@@ -0,0 +1,12 @@
+const style = document.createElement('style')
+style.innerHTML = `
+  .css-nvf2q2 {
+    display: none;
+  }
+  .red-num--message { /* Bilibili red dot */
+    display: none;
+  }
+`
+
+document.head.appendChild(style)
+
